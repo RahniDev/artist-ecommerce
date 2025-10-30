988747309d0dc76b27d24efd02fc7c67a7b5877c
RahniDev <rahni32@protonmail.com>
2025-10-30 21:52:16 +0100

    Add category model

---
+++ backend/src/modules/category/category.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Types } from 'mongoose'
+
+interface ICategory {
+  name: string; 
+}
+
+const CategorySchema = new Schema<ICategory>({
+    name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32,
+    unique: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Category', CategorySchema);
