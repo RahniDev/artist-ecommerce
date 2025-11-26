cf851e98c7c53b5e0b9cd8d3933b95ff09fd1bc0
RahniDev <rahni32@protonmail.com>
2025-11-26 19:04:50 +0100

    Work on navbar

--- frontend/src/components/core/Navbar.tsx
+++ frontend/src/components/core/Navbar.tsx
@@ -1,45 +1,106 @@
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 import { useNavigate, NavLink } from 'react-router-dom'
+import { signout, isAuthenticated } from "../auth";
+import { itemTotal } from "./cartHelpers";
+import React from 'react';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
     const navigate = useNavigate();
+
+    const activeStyle = { color: "#ff7315" };
+    const defaultStyle = { color: "#3a3535" };
+
+    const handleSignout = () => {
+        signout(() => navigate("/"));
+    };
+    const auth = isAuthenticated();
+
     return (
-        <nav>
-            <ul>
+        <div>
+            <ul className="nav nav-items">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} width="50px" height="50px" alt="Logo" />
+                </a>
 
-
-                {/* {isAuthenticated() && ( */}
                 <li className="nav-item">
-                    <span
-                        className="nav-link"
-                        style={{ cursor: "pointer", color: "#3a3535" }}
-                    // onClick={() =>
-                    // signout(() => {
-                    //       navigate("/")
-                    //     })
-                    //   }
+                    <NavLink
+                        to="/"
+                        style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
                     >
-                        Signout
-                    </span>
+                        Home
+                    </NavLink>
                 </li>
-                {/* )} */}
 
-                <li><Person2OutlinedIcon /></li>
+                {auth && auth.user.role === 0 && (
+                    <li className="nav-item">
+                        <NavLink
+                            to="/user/dashboard"
+                            style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+                        >
+                            <Person2OutlinedIcon />
+                        </NavLink>
+                    </li>
+                )}
 
-                {/* if authenticated, show icon, else show signin/signup */}
-                <li><NavLink to="/cart"
-                    style={({ isActive }) => ({
-                        color: isActive ? '#000000' : '#ffffff'
-                    })}>
-                    <ShoppingCartOutlinedIcon />
-                    <sup>
-                        <small className="cart-badge"></small>
-                    </sup>
-                </NavLink></li>
+                {auth && auth.user.role === 1 && (
+                    <li className="nav-item">
+                        <NavLink
+                            to="/admin/dashboard"
+                            style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+                        >
+                            Dashboard
+                        </NavLink>
+                    </li>
+                )}
+
+                {!auth && (
+                    <>
+                        <li className="nav-item">
+                            <NavLink
+                                to="/signin"
+                                style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+                            >
+                                Signin
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                to="/signup"
+                                style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+                            >
+                                Signup
+                            </NavLink>
+                        </li>
+                    </>
+                )}
+
+                {auth && (
+                    <li className="nav-item">
+                        <span
+                            className="nav-link"
+                            style={{ cursor: "pointer", color: "#3a3535" }}
+                            onClick={handleSignout}
+                        >
+                            Signout
+                        </span>
+                    </li>
+                )}
+
+                <li className="nav-item cart-icon">
+                    <NavLink
+                        to="/cart"
+                        style={{ color: "#ff7315" }}
+                    >
+                        <ShoppingCartOutlinedIcon name="shopping cart" />{" "}
+                        <sup>
+                            <small className="cart-badge">{itemTotal()}</small>
+                        </sup>
+                    </NavLink>
+                </li>
             </ul>
-        </nav>
-    )
-}
+        </div>
+    );
+};
 
 export default Navbar
