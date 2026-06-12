eaf701423db3c14164c5f1219e4581ddb9ea193e
RahniDev <rahni32@protonmail.com>
2026-06-12 12:18:26 +0200

    Fix cors error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,7 +29,10 @@ app.use((req, res, next) => {
   express.json()(req, res, next);
 });
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [ 
+    "http://localhost:5173",
+  "https://artist-ecommerce.vercel.app"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
