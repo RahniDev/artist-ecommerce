eab3f5bae0c554c77a8be7a837fd3e1f8e0c354a
RahniDev <rahni32@protonmail.com>
2026-07-27 17:40:17 +0200

    Correct analytics import

--- frontend/src/core/EntrancePage.tsx
+++ frontend/src/core/EntrancePage.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import hero from '../assets/hero.webp'
 import { Box, Typography } from '@mui/material'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const EntrancePage = () => {
     return (
